90db66ab7388e4759d335936905c2313930e8e7d
Arya Ansuman Priyadarshi <arya.priyadarshi@ymail.com>
2021-02-27 12:22:20 +1100

    First Commit

---
+++ src/main/java/springboot/code/pipeline/SpringbootCodePipelineApplication.java
@@ -0,0 +1,13 @@
+package springboot.code.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCodePipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCodePipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springboot/code/pipeline/SpringbootCodePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.code.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCodePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
